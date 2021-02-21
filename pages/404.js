fea41a2f68b44c50196ba344af1f0d332b811062
mvkump <mvkump@utu.fi>
2021-02-21 16:36:43 +0200

    404 page layout

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,9 @@
 import PageNotFoundTemplate from "../components/template/PageNotFoundTemplate";
 
+import DefaultLayout from "../components/module/Body/Default";
+import DefaultHeader from "../components/module/Header";
+
 export default PageNotFoundTemplate;
+
+PageNotFoundTemplate.Header = DefaultHeader;
+PageNotFoundTemplate.Layout = DefaultLayout;
