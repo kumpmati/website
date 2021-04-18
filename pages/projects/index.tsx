67a376bab7d926733e500ca0db116a6502ae5e32
mvkump <mvkump@utu.fi>
2021-04-18 16:08:40 +0300

    refactored individual page components

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,10 +1,34 @@
-import ProjectsPage, {
-  ProjectsPageProps,
-} from "@components/ProjectsPage/ProjectsPage";
-import { ENTRIES } from "@constants/entries";
+import { FC } from "react";
+import { CTMarkdownContent } from "@type/content";
+import Header from "@components/Header/Header";
+import MarkdownContent from "@components/MarkdownContent";
+import Page from "@components/Page/Page";
+import { Entry, EntryCollection } from "contentful";
+import List from "@components/List";
 import { getEntriesOfType, getEntry } from "@services/contentful/util";
+import { ENTRIES } from "@constants/entries";
+
+const ProjectsPage: FC<ProjectsPageProps> = ({ markdown, projects }) => {
+  const { title, markdownContent } = markdown.fields;
+
+  return (
+    <Page title={`MK | ${title}`}>
+      <Header>
+        <h3>Projects</h3>
+        <MarkdownContent content={markdownContent} />
+      </Header>
+
+      <List collection={projects} />
+    </Page>
+  );
+};
 
 export default ProjectsPage;
+
+export interface ProjectsPageProps {
+  markdown: Entry<CTMarkdownContent>;
+  projects: EntryCollection<any>;
+}
 
 export async function getStaticProps() {
   const props: ProjectsPageProps = {
