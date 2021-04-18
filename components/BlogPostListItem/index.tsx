b215ccc14582ab2ddf72c06dc570cb2559dd669b
mvkump <mvkump@utu.fi>
2021-04-18 16:36:26 +0300

    list animation

--- components/BlogPostListItem/index.tsx
+++ components/BlogPostListItem/index.tsx
@@ -7,12 +7,18 @@ import css from "./BlogPostListItem.module.css";
 
 const BlogPostListItem: FC<BlogPostListItemProps> = ({ entry }) => {
   const { title, summary, slug } = entry.fields;
+  const createDate = new Date(entry.sys.createdAt).toLocaleDateString();
 
   return (
     <Link href={`/blog/${slug}`}>
       <a className={css.post}>
-        <p className={css.post__title}>{title}</p>
-        <p className={css.post__summary}>{summary}</p>
+        <div>
+          <p className={css.post__title}>{title}</p>
+          <p className={css.post__summary}>{summary}</p>
+        </div>
+        <div>
+          <p className={css.post__created}>{createDate}</p>
+        </div>
       </a>
     </Link>
   );
