226ccaea23d2aec474a586936433358e7c899039
mvkump <mvkump@utu.fi>
2021-04-19 12:25:51 +0300

    added markdown link style

--- constants/colorSchemes.ts
+++ constants/colorSchemes.ts
@@ -10,6 +10,7 @@ export const ThemeContext = createContext<{
 export const DARK_THEME = {
   "--txt-col": "white",
   "--txt-col-subtle": "gray",
+  "--link-col": "white",
   "--bg-col": "black",
   "--code-txt-col": "rgb(170,170,170)",
   "--code-bg-col": "rgba(255,255,255,0.1)",
@@ -18,6 +19,7 @@ export const DARK_THEME = {
 export const LIGHT_THEME = {
   "--txt-col": "black",
   "--txt-col-subtle": "gray",
+  "--link-col": "black",
   "--bg-col": "white",
   "--code-txt-col": "rgb(70,70,70)",
   "--code-bg-col": "rgba(0,0,0,0.1)",
