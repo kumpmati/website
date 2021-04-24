1323800544935161d65f57f68042341b9fa21feb
mvkump <mvkump@utu.fi>
2021-04-24 21:21:17 +0300

    Adjusted code snippet bg color

--- constants/colorSchemes.ts
+++ constants/colorSchemes.ts
@@ -12,7 +12,7 @@ export const DARK_THEME = {
   "--txt-col-subtle": "rgb(160,160,160)",
   "--link-col": "white",
   "--bg-col": "black",
-  "--overlay": "rgba(255,255,255,0.1)",
+  "--overlay": "rgba(255,255,255,0.05)",
   "--code-txt-col": "rgb(170,170,170)",
   "--code-bg-col": "rgba(255,255,255,0.1)",
 };
@@ -22,7 +22,7 @@ export const LIGHT_THEME = {
   "--txt-col-subtle": "rgb(110,110,110)",
   "--link-col": "black",
   "--bg-col": "white",
-  "--overlay": "rgba(0,0,0,0.1)",
+  "--overlay": "rgba(0,0,0,0.05)",
   "--code-txt-col": "rgb(70,70,70)",
   "--code-bg-col": "rgba(0,0,0,0.1)",
 };
