21942036c386dc467707d288d808cb7ce596773c
mvkump <mvkump@utu.fi>
2021-05-20 22:58:42 +0300

    fixed linear gradient on safari

--- constants/colorSchemes.ts
+++ constants/colorSchemes.ts
@@ -1,17 +1,19 @@
 import { createContext } from "react";
 
-export const ThemeContext = createContext<{
-  theme: any;
-  current: "dark" | "light" | "auto";
-  dark: boolean;
-  setColorScheme: (value: "dark" | "light" | "auto") => any;
-} | null>(null);
+export const ThemeContext =
+  createContext<{
+    theme: any;
+    current: "dark" | "light" | "auto";
+    dark: boolean;
+    setColorScheme: (value: "dark" | "light" | "auto") => any;
+  } | null>(null);
 
 export const DARK_THEME = {
   "--txt-col": "white",
   "--txt-col-subtle": "rgb(160,160,160)",
   "--link-col": "white",
-  "--bg-col": "black",
+  "--bg-col": "#000",
+  "--bg-col-transparent": "rgba(0,0,0,0)",
   "--overlay": "rgba(255,255,255,0.08)",
   "--code-txt-col": "rgb(170,170,170)",
   "--code-bg-col": "rgba(255,255,255,0.1)",
@@ -21,7 +23,8 @@ export const LIGHT_THEME = {
   "--txt-col": "black",
   "--txt-col-subtle": "rgb(110,110,110)",
   "--link-col": "black",
-  "--bg-col": "white",
+  "--bg-col": "#fff",
+  "--bg-col-transparent": "rgba(255,255,255,0)",
   "--overlay": "rgba(0,0,0,0.05)",
   "--code-txt-col": "rgb(70,70,70)",
   "--code-bg-col": "rgba(0,0,0,0.1)",
