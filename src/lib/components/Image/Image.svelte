113b8ff40a4e13ef150ecd9676eafb9f1a0b91e3
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2022-01-03 01:41:27 +0200

    custom image component

---
+++ src/lib/components/Image/Image.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	export let src: string;
+	export let width: number = 100;
+	export let height: number = 100;
+	export let alt: string = '';
+	export let loading: 'lazy' | 'eager' = 'eager';
+	export let style: string = '';
+
+	let className;
+	export { className as class };
+
+	const breakpoints = [500, 900, 1300, 1700];
+</script>
+
+<picture>
+	<source
+		type="image/jpeg"
+		srcset={breakpoints.map((w) => `${src}?w=${w}&fm=jpeg ${w}w`).join(',')}
+	/>
+	<source
+		type="image/webp"
+		srcset={breakpoints.map((w) => `${src}?w=${w}&fm=webp ${w}w`).join(',')}
+	/>
+
+	<img class={className} {loading} decoding="async" {src} {width} {height} {alt} {style} />
+</picture>
