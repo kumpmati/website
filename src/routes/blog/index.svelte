6b7e5003dbbe0bbf0c985be6c78e1ed284c9503f
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2022-04-12 04:45:43 +0300

    small blog styling

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <div class="container">
-	<h1>Blog</h1>
+	<h1>blog</h1>
 
 	<ul>
 		{#each posts as post (post.sys.id)}
@@ -32,9 +32,32 @@
 	h1 {
 		font-size: 64px;
 		margin: 0;
+		margin-bottom: 2rem;
+		letter-spacing: -0.07em;
 
 		@media screen and (max-width: 900px) {
 			font-size: 50px;
+		}
+	}
+
+	ul {
+		list-style: none;
+		padding: 0;
+		display: flex;
+		flex-direction: column;
+
+		li > a {
+			display: block;
+			font-size: 22px;
+			font-weight: 900;
+			text-decoration: none;
+			color: inherit;
+			opacity: 0.6;
+			margin-bottom: 1rem;
+
+			&:hover {
+				opacity: 1;
+			}
 		}
 	}
 </style>
