89b90e01e21abd1ad3c1b430c70bbbe992a10b17
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2023-07-13 01:20:11 +0300

    disable cursor on touch devices

--- src/lib/Cursor.svelte
+++ src/lib/Cursor.svelte
@@ -1,25 +1,29 @@
 <script lang="ts">
 	import { spring } from 'svelte/motion';
+	import { isTouchDevice } from './touch';
+	import { onMount } from 'svelte';
 
 	let enabled = false;
+	let visible = false;
 	const size = spring(1, { stiffness: 0.04, damping: 0.25, precision: 0.001 });
 
-	const pos = spring(
-		{ x: 1920 / 2, y: 1080 / 2 },
-		{ stiffness: 0.04, damping: 0.25, precision: 0.001 }
-	);
+	const pos = spring({ x: 0, y: 0 }, { stiffness: 0.04, damping: 0.25, precision: 0.001 });
+
+	onMount(() => {
+		if (!isTouchDevice()) enabled = true;
+	});
 </script>
 
 <svelte:window
 	on:mousemove={(e) => {
-		enabled = true;
+		visible = true;
 		pos.set({ x: e.clientX, y: e.clientY });
 	}}
 	on:mousedown={() => size.set(0.7)}
 	on:mouseup={() => size.set(1)}
 />
 
-{#if enabled}
+{#if visible && enabled}
 	<circle
 		r={$size * 50}
 		stroke="#eee"
