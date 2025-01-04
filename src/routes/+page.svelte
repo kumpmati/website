f37672774860973cfd4c9a0b6bc38745638d7fec
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2025-01-04 22:46:58 +0200

    initial website version

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<svelte:head>
+	<title>matias kumpulainen</title>
+
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Matias Kumpulainen" />
+	<meta property="og:description" content="I like doing stuff." />
+</svelte:head>
+
+<div class="grid h-full w-full place-content-center">
+	<main class="flex flex-col items-center gap-4 p-4">
+		<div class="relative [&>#cheese-pointer]:hover:visible [&>#cheese-pointer]:hover:opacity-100">
+			<img src="/cheese.webp" alt="a 3d render of cheese" class="w-20 sm:w-28" />
+			<img
+				id="cheese-pointer"
+				src="/pointer.svg"
+				alt=""
+				class="invisible absolute right-1 top-1 -translate-y-full translate-x-full opacity-0 transition-all"
+			/>
+		</div>
+
+		<h1 class="text-center text-3xl sm:text-5xl">i like doing stuff.</h1>
+		<nav class="flex font-normal">
+			<a href="https://pieni.link/spotify" target="_blank" rel="noopener" class="hover:underline">
+				spotify
+			</a>
+			<a href="https://pieni.link/github" target="_blank" rel="noopener" class="hover:underline">
+				github
+			</a>
+			<a href="https://pieni.link/linkedin" target="_blank" rel="noopener" class="hover:underline">
+				linkedin
+			</a>
+		</nav>
+	</main>
+</div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,11 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				heading: ['Kalam', 'sans-serif']
+			}
+		}
 	},
 
 	plugins: [typography]
