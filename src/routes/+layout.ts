3a199d6182880e8385be40566d17755dae6961e1
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2025-01-04 23:00:41 +0200

    static adapter

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,3 +2,6 @@ import { dev } from '$app/environment';
 
 // disable hydration when in production (no interactivity needed)
 export const csr = dev;
+
+// prerender every page by default
+export const prerender = true;
