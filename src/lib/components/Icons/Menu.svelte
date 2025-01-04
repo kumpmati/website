722a661f74a080fdba7247055f4ed3b840d68d7f
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2025-01-04 22:15:30 +0200

    init sveltekit

--- src/lib/components/Icons/Menu.svelte
+++
@@ -1,4 +0,0 @@
-<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-	<path d="M3 7H20" stroke="currentColor" stroke-width="2" stroke-linecap="square" />
-	<path d="M3 17H20" stroke="currentColor" stroke-width="2" stroke-linecap="square" />
-</svg>
