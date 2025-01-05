fcb2f8b4326d8b20f103f23d26b24c049b635928
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2025-01-05 19:56:15 +0200

    simplify blog nav

--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -8,12 +8,10 @@
 	<title>blog</title>
 </svelte:head>
 
-<header class="flex w-full items-center justify-between p-4 sm:p-6">
+<header class="z-10 flex w-fit items-center justify-between p-4 sm:sticky sm:top-0 sm:p-6">
 	<a href="/" class="flex items-center gap-2 font-heading text-base font-bold">
 		<img src="/cheese.webp" alt="a 3d render of cheese" class="size-6" /> mk
 	</a>
-
-	<Nav />
 </header>
 
 <main class="mx-auto flex w-full max-w-2xl flex-col p-4 sm:p-6">
