235e0ef57cb6aadbf223ee1c6cac9828e81e6296
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2025-12-16 17:00:51 +0200

    add icons to nav

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,24 +1,29 @@
-<script>
-	import { page } from '$app/state';
+<script lang="ts">
+	import TablerBrandSpotify from '~icons/tabler/brand-spotify';
+	import TablerBrandGithub from '~icons/tabler/brand-github';
+	import TablerBrandLinkedin from '~icons/tabler/brand-linkedin';
+	import TablerPencilCode from '~icons/tabler/pencil-code';
+
+	const ITEMS = [
+		{ label: 'spotify', icon: TablerBrandSpotify, href: 'https://pieni.link/spotify' },
+		{ label: 'github', icon: TablerBrandGithub, href: 'https://pieni.link/github' },
+		{ label: 'linkedin', icon: TablerBrandLinkedin, href: 'https://pieni.link/linkedin' },
+		{ label: 'blog', icon: TablerPencilCode, href: '/blog' }
+	] as const;
 </script>
 
-<nav class="flex flex-wrap justify-center font-normal">
-	<a href="https://pieni.link/spotify" target="_blank" rel="noopener" class="hover:underline">
-		spotify
-	</a>
-	.
-	<a href="https://pieni.link/github" target="_blank" rel="noopener" class="hover:underline">
-		github
-	</a>
-	.
-	<a href="https://pieni.link/linkedin" target="_blank" rel="noopener" class="hover:underline">
-		linkedin
-	</a>
-	.
-	<a
-		href="/blog"
-		class={{ 'hover:underline': true, underline: page.url.pathname.startsWith('/blog') }}
-	>
-		blog
-	</a>
+<nav class="flex flex-wrap justify-center gap-4 font-normal">
+	{#each ITEMS as item (item.href)}
+		{@const relative = item.href.startsWith('/')}
+
+		<a
+			href={item.href}
+			target={!relative ? '_blank' : undefined}
+			rel={!relative ? 'noopener' : undefined}
+			class="flex items-center gap-0.5 outline-none hover:underline focus:underline"
+		>
+			<item.icon class="size-5" />
+			{item.label}
+		</a>
+	{/each}
 </nav>
